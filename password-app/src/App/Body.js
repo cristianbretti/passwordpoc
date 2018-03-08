e6dfbb7b070b2c2938e26c67185d976040ae44eb
Cristian <cristian.osoriobretti@gmail.com>
2018-03-08 16:35:52 +0100

    Added a body

---
+++ password-app/src/App/Body.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Body.css';
+
+class Body extends Component {
+  render() {
+    return (
+      <h3>I'm a body!</h3>
+    );
+  }
+}
+
+export default Body;
