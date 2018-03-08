e752b0b2cdf7f4ad4770d26163f522539c1adfa4
Cristian <cristian.osoriobretti@gmail.com>
2018-03-08 16:53:17 +0100

    Did a fetch for the data

--- password-app/src/App/Body.js
+++ password-app/src/App/Body.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 import './Body.css';
 
 class Body extends Component {
-  render() {
-    return (
-      <h3>I'm a body!</h3>
+	constructor(props) {
+    	super(props);
+    	this.state = {
+    		passwords: []
+    	};
+  	}
+
+
+	componentDidMount(){
+		var url = "https://kodprov.herokuapp.com/"
+
+		fetch(url).then(response => {
+			console.log(response.status)
+			return response.json();
+		}).then(data => {
+			this.setState({
+				passwords: data
+			})
+		})
+	}
+
+	render() {
+    	return (
+      	<h3>I'm a body!</h3>
     );
   }
 }
