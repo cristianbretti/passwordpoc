6b30139ad6df3a8c562427567fe1211cf2ac3cf5
Cristian <cristian.osoriobretti@gmail.com>
2018-03-08 16:32:00 +0100

    Moved out the header into own component

---
+++ password-app/src/App/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="Header">
+          <h1 className="Title">Lösenordshanterare</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
