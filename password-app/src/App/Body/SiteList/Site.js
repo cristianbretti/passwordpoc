31bb7cfa902d440623217baa779c82b8856ba5ff
Cristian <cristian.osoriobretti@gmail.com>
2018-03-08 18:20:23 +0100

    Added a popup that displays the encrypted password

--- password-app/src/App/Body/SiteList/Site.js
+++ password-app/src/App/Body/SiteList/Site.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import './Site.css';
 
 class Site extends Component {
 	constructor(props) {
     	super(props);
-    	
+    	this.state = {
+    		open: false
+    	}
+  	}
+
+  	handleOpen = () => {
+    	this.setState({open: true});
+  	};
+
+  	handleClose = () => {
+  		this.setState({open: false});
   	}
 
 	render() {
+		const buttons = [
+	      <FlatButton
+	        label="Decrypt"
+	        primary={true}
+	      />,
+	      <FlatButton
+	        label="Close"
+	        primary={true}
+	        onClick={this.handleClose}
+	      />
+    ];
 		return(
 			<tr>
-				<button>{this.props.siteInfo.site}</button>
+				<div>
+					<button onClick={this.handleOpen}>{this.props.siteInfo.site}</button>
+					<Dialog
+			          title={this.props.siteInfo.site}
+			          actions={buttons}
+			          modal={false}
+			          open={this.state.open}
+			          onRequestClose={this.handleClose}
+			        >
+			          {this.props.siteInfo.password}
+			        </Dialog>
+				</div>
+				
 			</tr>
 		)
     }
