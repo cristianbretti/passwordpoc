a97837fb8439bd963be472f67239c1eb208c523e
Cristian <cristian.osoriobretti@gmail.com>
2018-03-12 11:51:56 +0100

    Added a bad test for the fetching of data

--- password-app/src/App.test.js
+++ password-app/src/App.test.js
@@ -2,8 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+//This should be implemented with mocks instead! To do the test with an actual fetch is risky
+//but I didn't have time to do a mock test.
+describe('get data using Promises', () => {
+  it('should fetch all the data', () => {
+    return fetch("https://kodprov.herokuapp.com/")
+    .then(response => {
+      expect(response).toBeDefined()
+      return response.json()
+    }).then(data => {
+    	expect(data.length).toEqual(62)
+    })
+  })
+
+  it('First data is google.com', () => {
+    return fetch("https://kodprov.herokuapp.com/")
+    .then(response => {
+      expect(response).toBeDefined()
+      return response.json()
+    }).then(data => {
+    	expect(data[0].site).toEqual("google.com")
+    })
+  })
+})
